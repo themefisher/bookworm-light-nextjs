6cee071414f00ed8a0698518473826423bf59d2d
tfsomrat <themefisher.dev@gmail.com>
2022-08-06 14:35:53 +0600

    fixed google fonts render blocking issue

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -1,9 +1,11 @@
 import config from "@config/config.json";
+import theme from "@config/theme.json";
 import { plainify } from "@lib/utils/textConverter";
 import Footer from "@partials/Footer";
 import Header from "@partials/Header";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Base = ({
   title,
@@ -17,6 +19,19 @@ const Base = ({
   const { meta_image, meta_author, meta_description } = config.metadata;
   const { base_url } = config.site;
   const router = useRouter();
+
+  // import google font css
+  const pf = theme.fonts.font_family.primary;
+  const sf = theme.fonts.font_family.secondary;
+  const [fontcss, setFontcss] = useState();
+  useEffect(() => {
+    fetch(
+      `https://fonts.googleapis.com/css2?family=${pf}${
+        sf ? "&family=" + sf : ""
+      }&display=swap`
+    ).then((res) => res.text().then((css) => setFontcss(css)));
+  });
+
   return (
     <>
       <Head>
@@ -26,6 +41,18 @@ const Base = ({
             meta_title ? meta_title : title ? title : config.site.title
           )}
         </title>
+
+        {/* google font css */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `${fontcss}`,
+          }}
+        />
 
         {/* canonical url */}
         {canonical && <link rel="canonical" href={canonical} itemProp="url" />}
--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,14 @@
 import TwSizeIndicator from "@components/TwSizeIndicator";
 import config from "@config/config.json";
-import theme from "@config/theme.json";
 import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   const { favicon } = config.site;
-  const pf = theme.fonts.font_family.primary;
-  const sf = theme.fonts.font_family.secondary;
+
   return (
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" href={favicon} />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href={`https://fonts.googleapis.com/css2?family=${pf}${
-            sf ? "&family=" + sf : ""
-          }&display=swap`}
-          rel="stylesheet"
-        />
       </Head>
 
       <body>
