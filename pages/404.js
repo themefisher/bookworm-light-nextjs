9d6bbe994129188967eb7c7130b40cf38fb007d9
tfsomrat <themefisher.dev@gmail.com>
2022-08-13 10:18:31 +0600

    added 404.md

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,33 @@
 import Base from "@layouts/Baseof";
+import { getRegularPage } from "@lib/contents";
+import { markdownify } from "@lib/utils/textConverter";
 
-const notFound = () => {
+const notFound = ({ data }) => {
+  const { frontmatter, content } = data;
   return (
     <Base>
       <section className="section">
         <div className="container">
           <div className="flex h-[40vh] items-center justify-center">
             <div className="text-center">
-              <h1 className="mb-4">Error 404</h1>
-              <p className="text-h2">Page Not Found!!</p>
+              <h1 className="mb-4">{frontmatter.title}</h1>
+              {markdownify(content, "div", "content prose-headings:text-text")}
             </div>
           </div>
         </div>
       </section>
     </Base>
   );
+};
+
+// for regular page data
+export const getStaticProps = async () => {
+  const notFoundData = await getRegularPage();
+  return {
+    props: {
+      data: notFoundData,
+    },
+  };
 };
 
 export default notFound;
