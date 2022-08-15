0e8bccf450beb6c167d8c013c958edbca9885451
tfsomrat <themefisher.dev@gmail.com>
2022-08-15 09:11:28 +0600

    change google font loading location

--- layouts/Baseof.js
+++ layouts/Baseof.js
@@ -1,11 +1,9 @@
 import config from "@config/config.json";
-import theme from "@config/theme.json";
 import { plainify } from "@lib/utils/textConverter";
 import Footer from "@partials/Footer";
 import Header from "@partials/Header";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 
 const Base = ({
   title,
@@ -17,20 +15,8 @@ const Base = ({
   children,
 }) => {
   const { meta_image, meta_author, meta_description } = config.metadata;
-  const { base_url, favicon } = config.site;
+  const { base_url } = config.site;
   const router = useRouter();
-
-  // import google font css
-  const pf = theme.fonts.font_family.primary;
-  const sf = theme.fonts.font_family.secondary;
-  const [fontcss, setFontcss] = useState();
-  useEffect(() => {
-    fetch(
-      `https://fonts.googleapis.com/css2?family=${pf}${
-        sf ? "&family=" + sf : ""
-      }&display=swap`
-    ).then((res) => res.text().then((css) => setFontcss(css)));
-  });
 
   return (
     <>
@@ -42,29 +28,8 @@ const Base = ({
           )}
         </title>
 
-        {/* favicon */}
-        <link rel="shortcut icon" href={favicon} />
-
-        {/* google font css */}
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `${fontcss}`,
-          }}
-        />
-
         {/* canonical url */}
         {canonical && <link rel="canonical" href={canonical} itemProp="url" />}
-
-        {/* responsive meta */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=5"
-        />
 
         {/* noindex robots */}
         {noindex && <meta name="robots" content="noindex,nofollow" />}
